a1da1fa4d63969f9a305b2fd0357bea3c3f37ef8
physcatcat <Pavlichenko2001@yandex.ru>
2023-03-03 20:24:17 +0500

    Creating CRUD
    step 2

--- src/main/java/ru/ivanov/springmvc/controllers/PeopleController.java
+++ src/main/java/ru/ivanov/springmvc/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.ivanov.springmvc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.ivanov.springmvc.dao.PersonDAO;
+import ru.ivanov.springmvc.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -26,7 +25,37 @@ public class PeopleController {
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("person", personDAO.show(id));
+        model.addAttribute("person", personDAO.show(id));;
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
     }
 }
--- src/main/java/ru/ivanov/springmvc/dao/PersonDAO.java
+++ src/main/java/ru/ivanov/springmvc/dao/PersonDAO.java
@@ -8,15 +8,14 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int size = 0;
+    private static int PEOPLE_COUNT = 0;
     private List<Person> people;
-
     {
         people = new ArrayList<>();
-        people.add(new Person(++size, "Alina"));
-        people.add(new Person(++size, "Pavel"));
-        people.add(new Person(++size, "Misha"));
-        people.add(new Person(++size, "Gleb"));
+        people.add(new Person(++PEOPLE_COUNT, "Alina"));
+        people.add(new Person(++PEOPLE_COUNT, "Pavel"));
+        people.add(new Person(++PEOPLE_COUNT, "Misha"));
+        people.add(new Person(++PEOPLE_COUNT, "Gleb"));
     }
 
     public List<Person> index() {
@@ -25,5 +24,19 @@ public class PersonDAO {
 
     public Person show(int id) {
         return people.stream().filter(p -> p.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person person) {
+        Person personToUpdate = show(id);
+        personToUpdate.setName(person.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
